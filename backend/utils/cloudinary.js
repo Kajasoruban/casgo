c4cee99a5082d6c69967c3b1ddc8c0e21ea67dd9
Kajasoruban <kajan.bluwin@gmail.com>
2024-01-04 16:41:38 +0530

    some

---
+++ backend/utils/cloudinary.js
@@ -0,0 +1,9 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'dbczgzhd3', 
+  api_key: '954188386282655', 
+  api_secret: 'iamkajan' 
+});
+
+export default cloudinary;
